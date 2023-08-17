e1945fc6b8dd9ca1f939335aa327ddc1e78c37bb
samuel-soto2 <1461888@senati.pe>
2023-08-17 14:35:45 -0500

    Opracion

---
+++ multiplicacion/src/main/java/com/mycompany/multiplicacion/Multiplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multiplicacion;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Multiplicacion {
+
+    public static void main(String[] args) {
+        case 3:
+             int num5,num6,mult;
+             System.out.print("INGRESE EL PRIMER NUMERO: ");
+             num5=entrada.nextInt();
+             
+             System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+             num6=entrada.nextInt();
+             
+             mult=num5*num6;
+             System.out.print("\nLA MULTIPLICACION ES: "+mult);
+             break;
+    }
+}
