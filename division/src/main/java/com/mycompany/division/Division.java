e0135860e9b038a409fdc4f729e2d0cc3c030572
Eduardo9494 <1433341@senati.pe>
2023-08-17 14:38:05 -0500

    Acontinuacion el codigo de division

---
+++ division/src/main/java/com/mycompany/division/Division.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.division;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Division {
+
+    public static void main(String[] args) {
+        
+        
+        case 4:
+            int num7,num8,div;
+            System.out.print("INGRESE EL PRIMER NUMERO: ");
+            num7=entrada.nextInt();
+            
+            System.out.print("INGRESE EL PRIMER NUMERO: ");
+            num8=entrada.nextInt();
+            
+            div=num7/num8;
+            System.out.print("\nLA DIVISON ES: "+div);
+                    break;
+    }
+}
